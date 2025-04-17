dfc9e4e11da15b73f1eec526f4a9817e4c7d3e44
markgomer <aureliojuniorcmrj@hotmail.com>
2025-04-17 11:15:35 -0300

    Create route for payment checkout

---
+++ app/api/stripe/create-pay-checkout/route.ts
@@ -0,0 +1,37 @@
+import stripe from "@/lib/stripe";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    // não passa preço de produto ou qualquer informação sensível
+    // sob hipótese alguma
+    const { testID, userEmail } = await req.json() // pega o body da requisição
+
+    const price = process.env.STRIPE_PRODUCT_PRICE_ID;
+    if(!price) {
+        return NextResponse.json({ error: "Price not found" }, { status: 500 });
+    }
+
+    const metadata = { testID };
+
+    try {
+        const session = await stripe.checkout.sessions.create({
+            line_items: [{ price, quantity: 1 }],
+            mode: "payment",
+            payment_method_types: ["card", "paypal", "boleto"],
+            success_url: `${req.headers.get("origin")}/success`,
+            cancel_url: `${req.headers.get("origin")}/`,
+            ...(userEmail && { customer_email: userEmail })
+        })
+        if(!session.url) {
+            return NextResponse.json(
+                { error: "Session URL not found" },
+                { status: 500 }
+            );
+        }
+
+        return NextResponse.json({ sessionId: session.id }, { status: 200 });
+    } catch(error) {
+        console.error(error);
+    }
+
+}
