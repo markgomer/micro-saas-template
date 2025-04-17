a0c5984a63922dca71689436f990640285e3d2f4
markgomer <aureliojuniorcmrj@hotmail.com>
2025-04-17 18:38:07 -0300

    Add simple handlers for stripe payment, new subscription and cancelations

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -1,37 +1,64 @@
-import stripe from "@/lib/stripe";
-import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import { headers } from "next/headers";
+
+import stripe from "@/lib/stripe";
+import { handleStripePayment } from "@/server/stripe/handle-payment";
+import { handleStripeSubscription } from "@/server/stripe/handle-subscription";
+import { handleStripeCancelSubscription } from "@/server/stripe/handle-cancel";
 
 const secret = process.env.STRIPE_WEBHOOK_SECRET;
 
 export async function POS(req: NextRequest) {
-    const body = await req.json();
-    const headersList = await headers();
-    const signature = headersList.get("stripe-signature");
-    if(!signature || !secret) {
-        return NextResponse.json(
-            { error: "No signature or secret" },
-            { status: 400 }
-        )
-    }
+    try {
+        const body = await req.json();
+        const headersList = await headers();
+        const signature = headersList.get("stripe-signature");
+        if(!signature || !secret) {
+            return NextResponse.json(
+                { error: "No signature or secret" },
+                { status: 400 }
+            )
+        }
 
-    // isso vai ter que fazer manualmente pro mercado pago
-    const event = stripe.webhooks.constructEvent(body, signature, secret);
-    switch(event.type) {
-        case "checkout.session.completed": // pg realizado se status = paid
-            break;
-        case "checkout.session.expired": // expirou tempo de pagamento
-            break;
-        case "checkout.session.async_payment_succeeded": // boleto pago
-            break;
-        case "checkout.session.async_payment_failed": // boleto falhou
-            break;
-        case "customer.subscription.created": // criou assinatura
-            break;
-        case "customer.subscription.updated": // atualizou assinatura
-            break;
-        case "customer.subscription.deleted": // cancelou assinatura
-            break;
+        // isso vai ter que fazer manualmente pro mercado pago
+        const event = stripe.webhooks.constructEvent(body, signature, secret);
+        switch(event.type) {
+            // pode ser tanto pg unico quanto assinatura
+            case "checkout.session.completed": // pg realizado se status = paid
+                const metadata = event.data.object.metadata;
+                if(metadata?.price === process.env.STRIPE_PRODUCT_PRICE_ID) {
+                    await handleStripePayment(event)
+                }
+                if(metadata?.price === process.env.STRIPE_SUBSCRIPTION_PRICE_ID) {
+                    await handleStripeSubscription(event);
+                }
+                break;
+            case "checkout.session.expired": // expirou tempo de pagamento
+                console.log("Enviar um email para o usuário avisando que o pagamento expirou");
+                break;
+            case "checkout.session.async_payment_succeeded": // boleto pago
+                console.log("Enviar um email para o usuário avisando que o pagamento foi realizado");
+                break;
+            case "checkout.session.async_payment_failed": // boleto falhou
+                console.log("Enviar um email para o usuário avisando que o pagamento foi falhou");
+                break;
+            case "customer.subscription.created": // criou assinatura
+                console.log("Mensagem de boas vindas porque acabou de assinar");
+                break;
+            // raro... pode apagar
+            case "customer.subscription.updated": // atualizou assinatura
+                console.log("Algo mudou na sua assinatura. Verifique");
+                break;
+            case "customer.subscription.deleted": // cancelou assinatura
+                await handleStripeCancelSubscription(event);
+                break;
+        }
+    } catch(error) {
+        console.error(error);
+        return NextResponse.json(
+            { error: "Internal server error" },
+            { status: 500 }
+        );
     }
 
 }
