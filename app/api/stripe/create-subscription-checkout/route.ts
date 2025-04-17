30e9ccbb330b82921ab99b85746c3c4913b50355
markgomer <aureliojuniorcmrj@hotmail.com>
2025-04-17 11:34:43 -0300

    Create route for subscription payment

---
+++ app/api/stripe/create-subscription-checkout/route.ts
@@ -0,0 +1,41 @@
+import stripe from "@/lib/stripe";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    // não passa preço de produto ou qualquer informação sensível
+    // sob hipótese alguma
+    const { testID, userEmail } = await req.json() // pega o body da requisição
+
+    const price = process.env.STRIPE_PRODUCT_PRICE_ID;
+    if(!price) {
+        return NextResponse.json({ error: "Price not found" }, { status: 500 });
+    }
+
+    // metadata para passar info do chekcout até o webhook
+    const metadata = { testID };
+
+    try {
+        const session = await stripe.checkout.sessions.create({
+            line_items: [{ price, quantity: 1 }],
+            mode: "subscription",
+            payment_method_types: ["card"],
+            success_url: `${req.headers.get("origin")}/success`,
+            cancel_url: `${req.headers.get("origin")}/`,
+            ...(userEmail && { customer_email: userEmail }),
+            metadata
+            // customer
+        })
+        if(!session.url) {
+            return NextResponse.json(
+                { error: "Session URL not found" },
+                { status: 500 }
+            );
+        }
+
+        return NextResponse.json({ sessionId: session.id }, { status: 200 });
+    } catch(error) {
+        console.error(error);
+        return NextResponse.error();
+    }
+
+}
