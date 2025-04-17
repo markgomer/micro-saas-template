6c7bfcf6bfa5c664bd272f5a8b5054217cc18c29
markgomer <aureliojuniorcmrj@hotmail.com>
2025-04-17 10:27:20 -0300

    Add function createPaymentStripeCheckout
    * To receive a checkout URL from stripe and redirect it to checkout

--- app/hooks/useStripe.ts
+++ app/hooks/useStripe.ts
@@ -13,6 +13,28 @@ export function useStripe() {
         }
         loadStripeAsync();
     }, []);
+
+    async function createPaymentStripeCheckout(checkoutData: any) {
+        if(!stripe) return;
+
+        try {
+            const response = await fetch("/api/stripe/create-pay-checkout", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(checkoutData)
+            })
+            const data = await response.json();
+            // recebe URL da requisição e redireciona pro checkout
+            await stripe.redirectToCheckout({ sessionId: data.id });
+        } catch (error) {
+            console.error(error);
+        }
+    }
     return {
+        createPaymentStripeCheckout
     }
 }
+
+const { createPaymentStripeCheckout } = useStripe()
