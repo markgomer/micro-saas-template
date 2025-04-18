f8641cd747ddd9c26ffb6d0f9c9ea3415effc377
markgomer <aureliojuniorcmrj@hotmail.com>
2025-04-18 12:11:18 -0300

    Add button to pay with mercadopago

--- app/(project)/pagamentos/page.tsx
+++ app/(project)/pagamentos/page.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import { useStripe } from "@/hooks/useStripe";
+import useMercadoPago from "@/hooks/useMercadoPago";
 
 export default function Pagamentos() {
     const {
         createPaymentStripeCheckout,
         createSubscriptionStripeCheckout,
-        handleCreateStripePortal
+        handleCreateStripePortal,
     } = useStripe();
+
+    const { createMercadoPagoCheckout } = useMercadoPago();
 
     return (
         <div className="flex flex-col gap-10 items-center justify-center h-screen">
             <h1 className="text-4xl font-bold">Pagamentos</h1>
 
+            {/* Criar pagamento stripe */}
             <button
                 className= "border rounded-md px-1"
                 onClick={() => createPaymentStripeCheckout({testId: "123"})}
@@ -20,6 +24,7 @@ export default function Pagamentos() {
                 Criar Pagamento Stripe
             </button>
 
+            {/* Criar inscrição pela Stripe */}
             <button
                 className= "border rounded-md px-1"
                 onClick={() => createSubscriptionStripeCheckout({
@@ -29,11 +34,23 @@ export default function Pagamentos() {
                 Criar Assinatura Stripe
             </button>
 
+            {/* Para cancelar assinaturas */}
             <button
                 className= "border rounded-md px-1"
                 onClick={handleCreateStripePortal}
             >
                 Criar Portal de Pagamentos
+            </button>
+
+            {/* Pagar via mercado pago */}
+            <button
+                className= "border rounded-md px-1"
+                onClick={() => createMercadoPagoCheckout({
+                    testId: "123",
+                    userEmail: "teste@teste.com"
+                })}
+            >
+                Criar Pagamento Mercado Pago
             </button>
         </div>
     )
