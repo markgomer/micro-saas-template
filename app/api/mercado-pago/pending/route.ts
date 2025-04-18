6c9a723febd1265241e96dbc7d677f3d024d4564
markgomer <aureliojuniorcmrj@hotmail.com>
2025-04-18 10:57:01 -0300

    Add pending (for pix) route at mercado pago api

---
+++ app/api/mercado-pago/pending/route.ts
@@ -0,0 +1,28 @@
+import { Payment } from "mercadopago";
+import { NextRequest, NextResponse } from "next/server";
+
+import mpClient from "@/lib/mercadopago";
+
+export async function GET(req: NextRequest) {
+    const { searchParams } = new URL(req.url);
+    const paymentId = searchParams.get("payment_id");
+    const testId = searchParams.get("external_reference")
+
+    if(!paymentId || !testId) {
+        return NextResponse.json(
+            { error: "Payment ID or test Id not found"},
+            { status: 404 }
+        );
+    }
+    const payment = new Payment(mpClient);
+    const paymentData = await payment.get({
+        id: paymentId
+    });
+    if(
+        paymentData.status === "approved" ||
+        paymentData.date_approved !== null
+    ) {
+        return NextResponse.redirect(new URL(`success`, req.url));
+    }
+    return NextResponse.redirect(new URL(`/`, req.url));
+}
