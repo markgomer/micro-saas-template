bf154d66629286ffe1234830f789b2d70da11f28
markgomer <aureliojuniorcmrj@hotmail.com>
2025-04-24 16:46:39 -0300

    [FIX] Fix build error unused variable
    * Also changed session variable names due to ambiguity

--- app/api/stripe/create-pay-checkout/route.ts
+++ app/api/stripe/create-pay-checkout/route.ts
@@ -4,14 +4,15 @@ import stripe from "@/lib/stripe";
 import { auth } from "@/lib/auth";
 
 export async function POST(req: NextRequest) {
-    const session = await auth();
-    if (!session?.user) {
+    const loginSession = await auth();
+    if (!loginSession?.user) {
         return new NextResponse("Unauthorized", { status: 401 });
     }
 
     // não passa preço de produto ou qualquer informação sensível
-    // sob hipótese alguma
-    const { testID, userEmail } = await req.json() // pega o body da requisição
+    // no body da requisição sob hipótese alguma
+    const userEmail = await req.json() // pega o body da requisição
+    // const userEmail = loginSession?.user?.email;
 
     const price = process.env.STRIPE_PRODUCT_PRICE_ID;
     if(!price) {
@@ -20,12 +21,12 @@ export async function POST(req: NextRequest) {
 
     // metadata para passar info do checkout até o webhook
     const metadata = {
-        testId: session?.user?.id,
-        userEmail: session.user.email,
+        testId: loginSession?.user?.id,
+        userEmail: loginSession.user.email,
         price: price
     };
     try {
-        const session = await stripe.checkout.sessions.create({
+        const stripeSession = await stripe.checkout.sessions.create({
             line_items: [{ price, quantity: 1 }],
             mode: "payment",
             payment_method_types: ["card", "boleto"],
@@ -34,13 +35,13 @@ export async function POST(req: NextRequest) {
             ...(userEmail && { customer_email: userEmail }),
             metadata
         })
-        if(!session.url) {
+        if(!stripeSession.url) {
             return NextResponse.json(
                 { error: "Session URL not found" },
                 { status: 500 }
             );
         }
-        return NextResponse.json({ sessionId: session.id }, { status: 200 });
+        return NextResponse.json({ sessionId: stripeSession.id }, { status: 200 });
     } catch(error) {
         console.error(error);
         return NextResponse.error();
